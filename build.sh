2cd063cae5735a467935f4b1fc62861fe1dcd8af
Jiří Stránský <jistr@jistr.com>
2024-06-10 23:25:44 +0200

    Use radicale port as uid too

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -euxo pipefail
 dnf -y install radicale3
 dnf clean all
 
-groupmod -g 2401 radicale
-usermod -u 2401 radicale
+groupmod -g 5232 radicale
+usermod -u 5232 radicale
 chown -R radicale:radicale /var/lib/radicale
