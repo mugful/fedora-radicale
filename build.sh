32b58f8e677655a131f816b0b9958acad78e0d6b
jistr <jistr@jistr.com>
2025-04-11 10:08:42 +0200

    Pre-create the radicale group and user

--- build.sh
+++ build.sh
@@ -2,9 +2,10 @@
 
 set -euxo pipefail
 
+groupadd -g 5232 radicale
+useradd -u 5232 -g 5232 radicale
+
 dnf -y install radicale3
 dnf clean all
 
-groupmod -g 5232 radicale
-usermod -u 5232 radicale
 chown -R radicale:radicale /var/lib/radicale
